086573c633086dce06bb52381c25606cc58146e6
Brandon Reich <31513789+brandonwreich@users.noreply.github.com>
2018-04-10 08:30:33 -0600

    created the frame

--- src/art/view/ArtFrame.java
+++ src/art/view/ArtFrame.java
@@ -2,7 +2,25 @@ package art.view;
 
 import javax.swing.JFrame;
 
+import art.controller.ArtController;
+
 public class ArtFrame extends JFrame
 {
-
+	private ArtPanel appPanel;
+	
+	public ArtFrame(ArtController app)
+	{
+		super();
+		appPanel = new ArtPanel(app);
+		
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setSize(1200, 700);
+		this.setContentPane(appPanel);
+		this.setTitle("Creating art in Java");
+		this.setVisible(true);
+	}
 }
